f3c057736ff2766bca6dafda14dd8fb81c4d63fe
Mahesh Asolkar <mahesh@mahesha.com>
2012-09-26 22:43:53 -0700

    Initial population

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenMarket::Application.config.secret_token = '364b7b356f7c6e35a55bd27f0fbd0ba33f90c803f5c6cee84b05b3c591237fca953535f85a4c0b7868e514d12e9fee7f640fa4fa24b15f359b7ca6e34d465b9b'
