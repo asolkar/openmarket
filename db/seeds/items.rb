d62f34ef61fc9d2cb5579a79517bd9a88ec2390b
Mahesh Asolkar <mahesh@mahesha.com>
2013-01-08 23:27:24 -0800

    Randomized :active attr value in items seeds

--- db/seeds/items.rb
+++ db/seeds/items.rb
@@ -9,6 +9,7 @@ item_id = 1
     ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"].each do |item_num|
       price_val = Random.rand(10..50)
       quant_val = Random.rand(1..7)
+      act_val = (Random.rand(1..3) == 1)
       shop = Shop.find(shop_id)
       Item.create :db_seeding => true,
                   :id => item_id,
@@ -16,7 +17,7 @@ item_id = 1
                   :description => "Incredible item #{item_num} in #{user_name} Shop #{shop_num}",
                   :price => price_val,
                   :quantity => quant_val,
-                  :active => true,
+                  :active => act_val,
                   :shop => shop,
                   :shop_id => shop_id
       puts "#{user_name} Shop #{shop_num}:#{shop_id} item #{item_num}:#{item_id}, P#{price_val}, Q#{quant_val}"
